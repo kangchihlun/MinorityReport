369f1a3b0b514c0b802debb59551a11f8dd2e29b
Kang Chih Lun <kangchihlun@gmail.com>
2018-12-11 05:06:35 +0800

    Module Linker fix , add local testnet
    Module Linker fix , add local testnet

--- migrations/2_MinorityReport.js
+++ migrations/2_MinorityReport.js
@@ -1,4 +1,8 @@
+var safeMath  = artifacts.require('./SafeMath.sol');
 var Minor = artifacts.require("./MinorityReport.sol");
-module.exports = function(deployer){
-    deployer.deploy(Minor);
+module.exports = function (deployer) {
+    deployer.deploy(safeMath).then(() => {
+        deployer.link(safeMath, Minor);
+        return deployer.deploy(Minor);
+    });
 };
--- truffle.js
+++ truffle.js
@@ -20,6 +20,11 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // Match any network id
+    },
     testnet: {
       provider: new HDWalletProvider(mnemonic,
         "http://testnet.dexon.org:8545"),
